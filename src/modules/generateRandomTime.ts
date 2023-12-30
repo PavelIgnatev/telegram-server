e89148982dd354bd49105b8a779c0443f71f818f
Pavel <palllkaignatev@gmail.com>
2023-12-30 22:45:09 +0400

    Обновление .gitignore

--- src/modules/generateRandomTime.ts
+++ src/modules/generateRandomTime.ts
@@ -1,6 +1,6 @@
 export const generateRandomTime = () => {
-  const minTime = 3600000 * 2; // 2 hours in milliseconds
-  const maxTime = 3600000 * 4; // 4 hours in milliseconds
+  const minTime = 3600000 * 24; // 2 hours in milliseconds
+  const maxTime = 3600000 * 24; // 4 hours in milliseconds
 
   const randomTime =
     Math.floor(Math.random() * (maxTime - minTime + 1)) + minTime;
